b385ceddf22b2ab5ad90e731b74b628ddb61d468
Salvador Marquez <imt.simm@hotmail.com>
2019-05-02 17:35:59 -0500

    initial project

---
+++ window-Shopper/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  window-Shopper
+//
+//  Created by Salvador Marquez on 5/2/19.
+//  Copyright © 2019 Citsa Digital. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ window-ShopperTests/window_ShopperTests.swift
@@ -0,0 +1,34 @@
+//
+//  window_ShopperTests.swift
+//  window-ShopperTests
+//
+//  Created by Salvador Marquez on 5/2/19.
+//  Copyright © 2019 Citsa Digital. All rights reserved.
+//
+
+import XCTest
+@testable import window_Shopper
+
+class window_ShopperTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
