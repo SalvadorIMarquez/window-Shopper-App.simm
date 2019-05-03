fe849dd5290ebfaf84977945cae117de49f45c97
Salvador Marquez <imt.simm@hotmail.com>
2019-05-03 09:15:18 -0500

    Se crea clase para las cajas de texto y se incluye un Unit Testing

---
+++ window-Shopper/Views/CurrencyTxtField.swift
@@ -0,0 +1,30 @@
+//
+//  CurrencyTxtField.swift
+//  window-Shopper
+//
+//  Created by Salvador Marquez on 5/2/19.
+//  Copyright © 2019 Citsa Digital. All rights reserved.
+//
+
+import UIKit
+
+class CurrencyTxtField: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
